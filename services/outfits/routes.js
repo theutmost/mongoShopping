6a53396fc74c9707375bbe6baf5f8a3232e3cb63
theutmost <wearetheutmost@gmail.com>
2017-11-01 18:27:08 +0100

    first commit delete btn for OFs

--- services/outfits/routes.js
+++ services/outfits/routes.js
@@ -19,31 +19,55 @@ router.get('/outfitspage', (req, res) => {
             var arrOfAllOutfits = [];
             // console.log(results)
 
+            let whatever = 0
             for (var i = 0; i < results.length; i++) {
                 console.log("loop " + i)
                 var arrOfIDs = results[i].articles;
                 // console.log(arrOfIDs);
+                res.locals.arty = []
 
-                Article.find({ _id: arrOfIDs }, function (err, articles) {
+                Article.find({ _id: arrOfIDs }, (err, articles) => {
                     if (err) {
                         console.log(err)
                     } else {
+                        whatever++
+                        //arrOfAllOutfits.push(articles)
+                        console.log(results.length)
+                        console.log(whatever)
                         arrOfAllOutfits.push(articles)
-                        if (i = results.length) {
+                        if (whatever == results.length) { // use == and not = bc that's assigning and not checking 
                             console.log("on right page render")
                             console.log(arrOfAllOutfits)
                             res.render("outfitspage", { data: arrOfAllOutfits })
+
                         }
                     }
                 })
 
             };
-
+            // console.log(res.locals.arty)
             // res.render("outfitspage", 
             // {outfitOfArts: results,  // 1st variable, data of user collections
             // myPicksOutfits: ""}); // 2nd variable, data of my picked collections
         };
     });
 })
+
+/* ------------------- DELETE - Outfits -------------------*/
+
+router.post("/deleteOutfit/:id", (req, res) => {
+    console.log("youre about to delete an OF");
+    var id = req.params.id;
+    console.log(id);
+
+    Outfits.findByIdAndRemove({ _id: id }, function (err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.redirect('/outfitspage');
+        };
+    });
+});
+
 
 module.exports = router;
